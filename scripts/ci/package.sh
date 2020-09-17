60cce2ce1c1051547bc5f0ca044cbda022b3b8e8
Jaume Singla Valls <singlavalls@mediamarktsaturn.com>
2020-09-17 20:23:41 +0000

    Preparing travis-ci o package and send it to atomator

---
+++ scripts/ci/package.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+COMMIT_MSG=$(git log -1 --pretty=%B)
+COMMIT_SUB=$(git log -1 --pretty=%s)
+COMMIT_HASH=$(git log -1 --pretty=%H)
+
+
+R CMD build EscolesCovid
+if [  $? != 0 ]; then
+    echo "Error packaging it"
+    exit -1
+fi
+
+FILE_GENERATED=$(ls -la | grep EscolesCovid | head -n 1)
+echo "$FILE_GENERATED"
+
+curl -F message="$COMMIT_MSG" -F hash=$COMMIT_HASH -F file=@./$FILE_GENERATED $SERVER/upload_version/$TOKEN > "out.html"
+if [  $? != 0 ]; then
+    echo "Error pushing the package"
+    exit -1
+fi
+
+out_content=$(cat out.html)
+if [ "$out_content" != "Upload" ]; then
+    echo "$out_content"
+    exit -1
+fi
