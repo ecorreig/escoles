11efd3a1b4bd38801a9a0ee8983fdfe0e9ea5ca0
eudald <eudald@correig.net>
2020-09-18 10:56:36 +0200

    WIP: devtools

--- scripts/ci/package.sh
+++ scripts/ci/package.sh
@@ -5,6 +5,7 @@ COMMIT_SUB=$(git log -1 --pretty=%s)
 COMMIT_HASH=$(git log -1 --pretty=%H)
 
 
+R CMD install devtools
 R CMD build EscolesCovid
 if [  $? != 0 ]; then
     echo "Error packaging it"
