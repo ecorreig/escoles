0eb58c31cbebfdf2ae3f16232c7f5c936b8e9d92
eudald <eudald@correig.net>
2020-09-21 08:30:58 +0200

    better docs

--- R/docs.R
+++ R/docs.R
@@ -21,15 +21,33 @@ també estan explicats més avall. El codi elaborat també és públic i el pode
 
 <h2> Variables epidemiològiques </h2>
 
-<p>Totes les dades epidemiològiques han estat calculades seguint les metodologies del grup biocomsc de la UPC, que podeu trobar aquí: https://biocomsc.upc.edu/en/shared/avaluacio_risc.pdf. </p>
+<p>Totes les dades epidemiològiques han estat calculades seguint les metodologies del grup biocomsc de la UPC, que podeu trobar aquí: <a href='https://biocomsc.upc.edu/en/shared/avaluacio_risc.pdf' target='_blank'>https://biocomsc.upc.edu/en/shared/avaluacio_risc.pdf</a>. </p>
+
+<h4> Risc de rebrot </h4>
+
+<p>El risc de rebrot, també anomenat índex de creixement potencial (EPG per les seves sigles en anglès) és calcula com la multiplicació de la rho7 i l'incidència acumulada a 14 dies (per 100.000 habitants). Utilitzem aquest índex com a principal ja que s'ha establert com el de referència en les comunicacions oficials i dels mitjans de comunicació de Catalunya. </p>
+
+<h4> Incidència acumulada a 14 dies </h4>
+
+<p> Aquest valor és simplement la suma de tots els casos positius confirmats per PCR dels últims 14 dies dividits per cada 100.000 habitants. </p>
+
+<h4> Rho 7 </h4>
+
+La rho_0, en models epidemiològics, és la taxa base de creixement. En el nostre cas aquest número no té massa sentit teòric, perquè les restriccions modifiquen molt la rho0 (és la seva feina) i per tant, a nivell teòric, no té sentit. És per això que des de la UPC han creat la rho experimenta, que calculen com a la suma de casos dels tres últims dies dividit per la suma de casos d'entre 5 i 8 dies anteriors al que es calcula. La rho_7 és aleshores la mitjana de la rho calculada d'aquesta manera dels últims 7 dies. </p>
+
+<h4> Guia de Harvard </h4>
+
+La guia de Harvard, que podeu trobar <a href = 'https://globalepidemics.org/wp-content/uploads/2020/07/pandemic_resilient_schools_briefing_72020.pdf' target='_blank'>aquí</a> dóna referències per tal de decidir si obrir les comunitats (en particular centres educatius) segons l'estat de la pandèmia i, en concret, segons el número de casos confirmats diaris per cada 100.000 habitants. El resum de la guia seria:</p>
+<ol> Verd: tots els centres educatius han d'obrir sempre que es compleixin els protocols de seguretat. </ol>
+<ol> Groc: Guarderies obertes i centres d'educació espeicial fins a 8 anys oberts. Primària fins a 8 anys i educació especial fins a 12 anys s'han d'obrir només si els protocols de seguretat es poden complir estríctament en tots els centres del territori afectat.</ol>
+<ol> Taronja: igual que l'anterior, però només si tots els centres del territori poden complir els protocols de forma totalment estricta.</ol>
+<ol> Vermell: tots els centres educatius del territori han d'estar tancats. </ol>
 
 <h2> Probabilitats </h2>
 
 <p> Pel que fa als càlculs de probabilitats de casos, s'han utilitzat models de Poisson amb lambda igual a la prevalença de la malaltia a 14 dies. </p>
 
 <strong>Nota: seguint aquest <a href = 'https://www.nature.com/articles/s41591-020-0962-9' target='_blank'>article</a> s'ha tingut en compte que els infants i adolescents tenen una incidència de la malaltia d'aproximadament el 40% que els adults. Aquest valor segurament vagi actualitzant-se a mesura que surten més investigacions.</strong>
-
-<p> Els valors de la guia de Harvard els podeu trobar <a href = 'https://www.nature.com/articles/s41591-020-0962-9' target='_blank'>aquí</a>.</p>
 
 <p> La resta de càlculs són bastant directes; si els voleu repassar, són als fitxers calcs.R i calc_funcs.R. </p>
 
