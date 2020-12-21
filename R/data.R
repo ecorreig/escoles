470a9ff5a703ce7cacfd76355a6b6c833ab08d0b
eudald <eudald@correig.net>
2020-12-21 08:53:04 +0100

    fix data format

--- R/data.R
+++ R/data.R
@@ -86,7 +86,8 @@ import_pop_data <- function() {
   pb
 }
 
-update_data <- function() {
+update_data_DEPRECATED <- function() {
+  # This was used when the webpage had the data in the old format
   # Get school covid status
   headers <- "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
   names(headers) <- "user-agent"
@@ -129,11 +130,86 @@ update_data <- function() {
   tot
 }
 
+
+import_covid_schools <- function() {
+  q <- "https://analisi.transparenciacatalunya.cat/resource/fk8v-uqfv.json"
+  read.socrata(q, stringsAsFactors = F) %>%
+    mutate(
+      datageneracio = lubridate::ymd(datageneracio)
+    )  %>% 
+    group_by(codcentre) %>%
+    slice(which.max(datacreacio)) %>%
+    rename_with(toupper)
+}
+
+update_data <- function() {
+  # Get school covid status
+  q <- "https://analisi.transparenciacatalunya.cat/resource/fk8v-uqfv.json"
+  df <- read.socrata(q, stringsAsFactors = F) %>%
+    mutate(
+      datageneracio = lubridate::ymd(datageneracio)
+    )  %>% 
+    group_by(codcentre) %>%
+    slice(which.max(datacreacio)) %>%
+    rename_with(toupper)
+  # Get shool data
+  es <- readxl::read_xlsx(file.path("data", "escoles_base.xlsx"), col_types = "text")
+  
+  # Merge
+  tot <- es %>% 
+    left_join(df,
+              by = c("Codi centre" = "CODCENTRE"),
+              suffix = c("", "")) %>%
+    mutate(
+      Estat = case_when(
+        ESTAT == "Confinat" ~ "Tancada",
+        GRUP_CONFIN > 0 ~ "Grups en quarantena",
+        TRUE ~ "Normalitat"
+      ),
+      `Grups en quarantena` = case_when(
+        is.na(GRUP_CONFIN) ~ 0.0,
+        TRUE ~ as.numeric(GRUP_CONFIN)
+      ),
+      DATAGENERACIO = lubridate::ymd(DATAGENERACIO)
+      
+    ) %>%
+    mutate(
+      ALUMN_CONFIN = as.numeric(ALUMN_CONFIN),
+      GRUP_CONFIN = as.numeric(GRUP_CONFIN),
+      DOCENT_CONFIN = as.numeric(DOCENT_CONFIN),
+      ALTRES_CONFIN = as.numeric(ALTRES_CONFIN),
+      ALUMN_POSITIU = as.numeric(ALUMN_POSITIU_ACUM),
+      PERSONAL_POSITIU = as.numeric(PERSONAL_POSITIU_ACUM),
+      ALTRES_POSITIU = as.numeric(ALTRES_POSITIU_ACUM),
+      ALUMN_POSITIU_VIG11 = as.numeric(ALUMN_POSITIU_VIG11),
+      PERSONAL_POSITIU_VIG11 = as.numeric(PERSONAL_POSITIU_VIG11),
+      ALTRES_POSITIU_VIG11 = as.numeric(ALTRES_POSITIU_VIG11),
+      Coordenades_GEO_X = as.numeric(gsub(",", ".", Coordenades_GEO_X, fixed = T)),
+      Coordenades_GEO_Y = as.numeric(gsub(",", ".", Coordenades_GEO_Y, fixed = T))
+    )
+  
+  # Check whether data is new
+  data_creacio <- lubridate::now()
+  
+  # Store if it's new
+  files <- list.files(file.path(getwd(), "data",  "daily"))
+  name <- make.names(paste0(data_creacio, ".csv"))
+  
+  # only save on local FIXME (ugly hack)
+  if (!name %in% files & Sys.info()["sysname"] == "Windows") { 
+    pa <- file.path(file.path(getwd(), "data", "daily", name))
+    write.csv(tot, pa, row.names = F)
+  }
+  
+  tot
+}
+
+
 # Import school data
 import_schools <- function() {
 
-  esc <- update_data()
-  esc %>%  rename_all(funs(make_ascii(names(esc)))) %>%
+  update_data() %>%  
+    rename_with(make_ascii) %>%
     mutate(Codi_municipi = as.character(Codi_municipi)) %>%
     mutate(Codi_municipi = ifelse(nchar(Codi_municipi) < 5, paste0("0", Codi_municipi), Codi_municipi))
 }
@@ -161,7 +237,8 @@ import_evo <- function() {
 update_evo <- function(df) {
   
   # Careful, this updates records from same datetime (TODO: think about this)
-  evo <- import_evo() %>% add_row(
+  evo <- import_evo() %>% 
+    add_row(
     Dia = lubridate::now(),
     `Casos.alumnes` = sum(df$ALUMN_POSITIU, na.rm = T),
     `Alumnes.confinats` = sum(df$ALUMN_CONFIN, na.rm = T),
